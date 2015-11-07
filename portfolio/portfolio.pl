f5ba4af734474916d5f9a575cbde366c1bf4d941
Jack Lowrie <school@jbl-macbookpro.local>
2015-11-07 14:11:00 -0600

    Starter perl file
    got perl script to execute on local server

---
+++ portfolio/portfolio.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+use strict;
+
+use DBI;
+my $dbuser="pps860";
+my $dbpasswd="zaM7in9Wf";
+
+#use Time::ParseDate;
+use CGI qw(:standard);
+
+
+
+
+print "Content-type:text/html\r\n\r\n";
+print "<!DOCTYPE html>";
+print "<html lang=\"en\">";
+print "<head>";
+print "<title>Portfolio Manager</title>";
+print "</head>";
+print "<body>";
+
+print "hello, world!";
+
+
+print "</body>";
+print "</html>";
