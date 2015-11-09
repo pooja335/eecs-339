638923589f18d77a5a0fe441ae555f202a40ed66
shulerhl <hollidayshuler2016@u.northwestern.edu>
2015-11-09 17:10:00 -0600

    commented out holding history

--- portfolio/pf.sql
+++ portfolio/pf.sql
@@ -68,24 +68,24 @@ create table historicalData (
 	constraint hist_unique UNIQUE(symbol, timstamp)
 );
 
-create table holdingHistory (
-	--
-	holding_symbol varchar(64) NOT NULL,			
-	--										
-	histdata_symbol varchar(64) NOT NULL,
-	-- timestamp is a built in variable in sql, so use name timstamp
-	timstamp varchar(64) NOT NULL,
-	--
-	user_email varchar(64) NOT NULL,
-	--
-	pf_name varchar(64) NOT NULL,
-	--
-	constraint holdH_ref FOREIGN KEY (histdata_symbol, timstamp) references historicalData(symbol, timstamp),
-	--
-	constraint holdH_ref2 FOREIGN KEY (holding_symbol, user_email, pf_name) references holdings(symbol, user_email, portfolio_name),
-	--
-	constraint holdH_unique UNIQUE(holding_symbol, histdata_symbol, timstamp, user_email, pf_name)
-);
+-- create table holdingHistory (
+-- 	--
+-- 	holding_symbol varchar(64) NOT NULL,			
+-- 	--										
+-- 	histdata_symbol varchar(64) NOT NULL,
+-- 	-- timestamp is a built in variable in sql, so use name timstamp
+-- 	timstamp varchar(64) NOT NULL,
+-- 	--
+-- 	user_email varchar(64) NOT NULL,
+-- 	--
+-- 	pf_name varchar(64) NOT NULL,
+-- 	--
+-- 	constraint holdH_ref FOREIGN KEY (histdata_symbol, timstamp) references historicalData(symbol, timstamp),
+-- 	--
+-- 	constraint holdH_ref2 FOREIGN KEY (holding_symbol, user_email, pf_name) references holdings(symbol, user_email, portfolio_name),
+-- 	--
+-- 	constraint holdH_unique UNIQUE(holding_symbol, histdata_symbol, timstamp, user_email, pf_name)
+-- );
 
 INSERT INTO pfusers (name, email, password) VALUES ('root', 'root@root.com', 'rootroot');
 
