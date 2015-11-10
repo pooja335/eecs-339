67a4cf6ddc925490514b326876da6a12ac2d8de8
Jack Lowrie <school@vpn-165-124-164-16.vpn.northwestern.edu>
2015-11-09 19:24:28 -0600

    Updated sql
    — Removed holdingHistory (redundant)
    
    — Renamed historicalData to RecentStocksDaily
    
    — changed create for RecentStocksDaily to match cs339.StocksDaily
    
    — started writing create view as HistoricalData (commented out since it
    doesn’t run)

--- portfolio/pf.sql
+++ portfolio/pf.sql
@@ -1,13 +1,13 @@
 delete from pfusers;
 delete from portfolios;
 delete from holdings;
-delete from holdingHistory;
-delete from historicalData;
+-- delete from holdingHistory;
+delete from RecentStocksDaily;
 
 commit;
 
-drop table historicalData;
-drop table holdingHistory;
+drop table RecentStocksDaily;
+-- drop table holdingHistory;
 drop table holdings;
 drop table portfolios;
 drop table pfusers;
@@ -49,11 +49,12 @@ create table holdings (
 	constraint holdings_unique UNIQUE(symbol, user_email, portfolio_name)
 );
 
-create table historicalData (
+
+create table RecentStocksDaily (
 	--
-	symbol varchar(64) NOT NULL unique,
+	symbol varchar2(16) NOT NULL unique,
 	--
-	timstamp varchar(64) NOT NULL unique, 	
+	timestamp number NOT NULL unique, 	
 	--
 	open number NOT NULL,
 	--
@@ -63,36 +64,36 @@ create table historicalData (
 	--
 	low number NOT NULL,
 	--
-	volume number,
+	volume number NOT NULL,
 	--
-	constraint hist_unique UNIQUE(symbol, timstamp)
+	constraint hist_unique UNIQUE(symbol, timestamp)
 );
 
-create table holdingHistory (
-	--
-	holding_symbol varchar(64) NOT NULL,			
-	--										
-	histdata_symbol varchar(64) NOT NULL,
-	-- timestamp is a built in variable in sql, so use name timstamp
-	timstamp varchar(64) NOT NULL,
-	--
-	user_email varchar(64) NOT NULL,
-	--
-	pf_name varchar(64) NOT NULL,
-	--
-	constraint holdH_ref FOREIGN KEY (histdata_symbol, timstamp) references historicalData(symbol, timstamp),
-	--
-	constraint holdH_ref2 FOREIGN KEY (holding_symbol, user_email, pf_name) references holdings(symbol, user_email, portfolio_name),
-	--
-	constraint holdH_unique UNIQUE(holding_symbol, histdata_symbol, timstamp, user_email, pf_name)
-);
+-- create table holdingHistory (
+-- 	holding_symbol varchar(64) NOT NULL,			
+-- 	histdata_symbol varchar(64) NOT NULL,
+-- 	-- timestamp is a built in variable in sql, so use name timstamp
+-- 	timstamp varchar(64) NOT NULL,
+-- 	user_email varchar(64) NOT NULL,
+-- 	pf_name varchar(64) NOT NULL,
+-- 	constraint holdH_ref FOREIGN KEY (histdata_symbol, timstamp) references historicalData(symbol, timstamp),
+-- 	constraint holdH_ref2 FOREIGN KEY (holding_symbol, user_email, pf_name) references holdings(symbol, user_email, portfolio_name),
+-- 	constraint holdH_unique UNIQUE(holding_symbol, histdata_symbol, timstamp, user_email, pf_name)
+-- );
 
+
+
+
+
+-- CREATE VIEW HistoricalData AS SELECT * FROM cs339.StocksDaily UNION SELECT * FROM RecentStocksDaily;
+
+
+
+
+
+--Make dummy data
 INSERT INTO pfusers (name, email, password) VALUES ('root', 'root@root.com', 'rootroot');
-
 INSERT INTO portfolios (user_email, name, cash_account) VALUES ('root@root.com', 'portfolio 1', 100.00);
-
 INSERT INTO holdings (symbol, user_email, portfolio_name, num_shares) VALUES ('APPL', 'root@root.com', 'portfolio 1', 1);
-
 --INSERT INTO holdingHistory (holding_symbol, histdata_symbol, timstamp, user_email, pf_name) VALUES ('APPL', 'APPL', '00:00:00', 'root@root.com', 'portfolio 1');
-
 --CREATE VIEW histData as recent_data UNION ALL historicalData;
